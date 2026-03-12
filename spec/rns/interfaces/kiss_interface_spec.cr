677d1ede10c5eb4a50d06e70cafc2a1e4b1391a1
James Tippett <james.tippett@gmail.com>
2026-03-12 15:52:44 +0700

    fix: use IO::FileDescriptor.set_blocking instead of #blocking=

--- spec/rns/interfaces/kiss_interface_spec.cr
+++ spec/rns/interfaces/kiss_interface_spec.cr
@@ -394,7 +394,7 @@ describe RNS::KISSInterface do
       # Create pipe for simulating serial I/O
       r, w = IO.pipe
       rd_fd = IO::FileDescriptor.new(r.fd)
-      rd_fd.blocking = false
+      IO::FileDescriptor.set_blocking(rd_fd.fd, false)
 
       iface.serial_io = rd_fd
       iface.running = true
@@ -784,7 +784,7 @@ describe RNS::KISSInterface do
       r, w = IO.pipe
 
       iface = RNS::KISSInterface.new(name: "kiss0", port: "/dev/x", open_port: false)
-      wr_fd = IO::FileDescriptor.new(w.fd).tap { |fd| fd.blocking = false }
+      wr_fd = IO::FileDescriptor.new(w.fd).tap { |fd| IO::FileDescriptor.set_blocking(fd.fd, false) }
       iface.serial_io = wr_fd
       iface.online = true
       iface.interface_ready = true
@@ -815,7 +815,7 @@ describe RNS::KISSInterface do
       r, w = IO.pipe
 
       iface = RNS::KISSInterface.new(name: "kiss0", port: "/dev/x", open_port: false)
-      wr_fd = IO::FileDescriptor.new(w.fd).tap { |fd| fd.blocking = false }
+      wr_fd = IO::FileDescriptor.new(w.fd).tap { |fd| IO::FileDescriptor.set_blocking(fd.fd, false) }
       iface.serial_io = wr_fd
       iface.online = true
       iface.interface_ready = true
@@ -1274,7 +1274,7 @@ describe RNS::AX25KISSInterface do
         ssid: 5,
         open_port: false
       )
-      wr_fd = IO::FileDescriptor.new(w.fd).tap { |fd| fd.blocking = false }
+      wr_fd = IO::FileDescriptor.new(w.fd).tap { |fd| IO::FileDescriptor.set_blocking(fd.fd, false) }
       iface.serial_io = wr_fd
       iface.online = true
       iface.interface_ready = true
